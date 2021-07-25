e58af20428b3909e36552c952bb6256768397764
Kucukdogru <m42erve@hotmail.com>
2021-07-25 09:14:07 +0300

    Readme dosyası güncellendi.

--- src/pages/JobPostingUnapprovedList.js
+++ src/pages/JobPostingUnapprovedList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Header, Icon } from "semantic-ui-react";
 import JobPostingService from "../services/jobPostingService";
-import { Button, Card,Label } from "semantic-ui-react";
+import { Button, Card, Label } from "semantic-ui-react";
 import { toast } from "react-toastify";
 
 export default function JobPostingUnapprovedList() {
@@ -15,25 +15,25 @@ export default function JobPostingUnapprovedList() {
       .then((result) => setJobPostings(result.data.data));
   }, []);
 
-  function activeJobPosting(id){
+  function activeJobPosting(id) {
     let jobPostingService = new JobPostingService();
     jobPostingService
-    .activeJobPosting(id)
-    .then(result => {
-      toast.success("Job posting activated successfully");
-    }, [])
+      .activeJobPosting(id)
+      .then(result => {
+        toast.success("Job posting activated successfully");
+      }, [])
   }
 
-  function closeJobPosting(id){
+  function closeJobPosting(id) {
     let jobPostingService = new JobPostingService();
     jobPostingService
-    .closeJobPosting(id)
-    .then(result => {
-      toast.success("Job posting successfully deactivated");
-    }, [])
+      .closeJobPosting(id)
+      .then(result => {
+        toast.success("Job posting successfully deactivated");
+      }, [])
   }
 
-  
+
   return (
     <div>
       <Header as='h3' block>
@@ -45,7 +45,7 @@ export default function JobPostingUnapprovedList() {
           <Card fluid>
             <Card.Content>
               <Icon name='location arrow' color="teal" right className="ui.icon"
-                size="big"  /> {jobPosting.cityName}       
+                size="big" /> {jobPosting.cityName}
 
               <Card.Header>
                 {jobPosting.jobPosition.position}
@@ -54,35 +54,35 @@ export default function JobPostingUnapprovedList() {
                 {jobPosting.employer.companyName}
               </Card.Meta>
               <Card.Description>
-               Web Address: {jobPosting.employer.webAddress}{" "}<br/>
-               Number of position: <strong>{jobPosting.numberOfPosition}</strong><br/>
-               Working Type: <strong>{jobPosting.workingType}</strong><br/>
-               Working Time: <strong>{jobPosting.workingTime}</strong><br/>
-               Status: {jobPosting.status == true ? <Label style={{backgroundColor: 'blue', alignSelf: 'flex-start', color:'#fff'}}>
-    Aktif
-</Label> : <Label style={{backgroundColor: 'red', alignSelf: 'flex-start', color:'#fff'}}>
-    Pasif
-</Label>}
-            </Card.Description>
+                Web Address: {jobPosting.employer.webAddress}{" "}<br />
+                Number of position: <strong>{jobPosting.numberOfPosition}</strong><br />
+                Working Type: <strong>{jobPosting.workingType}</strong><br />
+                Working Time: <strong>{jobPosting.workingTime}</strong><br />
+                Status: {jobPosting.status == true ? <Label style={{ backgroundColor: 'blue', alignSelf: 'flex-start', color: '#fff' }}>
+                  Aktif
+                </Label> : <Label style={{ backgroundColor: 'red', alignSelf: 'flex-start', color: '#fff' }}>
+                  Pasif
+                </Label>}
+              </Card.Description>
             </Card.Content>
             <Card.Content extra>
-            <Button.Group        
-            >
-              
-                <Button positive color="green" 
-                onClick={() => activeJobPosting(jobPosting.id)}
-                > 
-                <Icon name='check' />
+              <Button.Group
+              >
+
+                <Button positive color="green"
+                  onClick={() => activeJobPosting(jobPosting.id)}
+                >
+                  <Icon name='check' />
                   Approve
                 </Button>
-                <Button negative color="red" 
-                onClick={() => closeJobPosting(jobPosting.id)}
+                <Button negative color="red"
+                  onClick={() => closeJobPosting(jobPosting.id)}
                 > <Icon name='close' />
                   Decline
                 </Button>
-              </Button.Group>             
-    
-  
+              </Button.Group>
+
+
             </Card.Content>
           </Card>
         </Card.Group>
